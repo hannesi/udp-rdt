84b86385399dec037c0a0e8a7de17f1fc2549b16
Hannes Koivusipilä <hannesi@iki.fi>
2024-11-07 17:35:00 +0200

    Fixing an oopsie in tmux script

--- tmux.sh
+++ tmux.sh
@@ -4,7 +4,13 @@
 
 session_name="udp-rdt"
 
-tmux new-session -d -s $session_name
-tmux new-window -t $session_name -d
-tmux split-window -h -t $session_name:2.1
+tmux has-session -t $session_name 2>/dev/null
+
+if [ $? != 0 ]; then
+  tmux new-session -d -s $session_name
+  tmux new-window -t $session_name -d
+  tmux split-window -h -t $session_name:2.1
+fi
+
 tmux attach -t $session_name
+
