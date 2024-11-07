5450b7deec6a108c3dd82650b156bb7c80208c7f
Hannes Koivusipilä <hannesi@iki.fi>
2024-11-07 18:31:11 +0200

    Project base

---
+++ README.md
@@ -0,0 +1,11 @@
+# UDP-RDT
+
+To start the server
+```sh
+cargo run --bin server
+```
+
+To start the client
+```sh
+cargo run --bin client
+```
---
+++ config/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod server_info {
+    use std::net::Ipv4Addr;
+
+    pub const ADDRESS: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+    pub const PORT: u16 = 42069;
+}
