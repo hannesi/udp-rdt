615683ce2a0f782d5332c71f59bd2ecdc00bd85d
hannesi <hannesi@iki.fi>
2024-11-08 14:36:58 +0200

    revert script changes

--- tmux.sh
+++ tmux.sh
@@ -7,9 +7,9 @@ session_name="udp-rdt"
 tmux has-session -t $session_name 2>/dev/null
 
 if [ $? != 0 ]; then
-  tmux new-session -d -s $session_name 'nvim'
-  tmux new-window -t $session_name -d 'cargo run --bin server'
-  tmux split-window -h -t $session_name:2.1 'cargo run --bin client'
+  tmux new-session -d -s $session_name
+  tmux new-window -t $session_name -d
+  tmux split-window -h -t $session_name:2.1
 fi
 
 tmux attach -t $session_name
